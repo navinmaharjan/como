750ba3d9e08e8f4b84bf7eb5c1e8a3c3a5f3f9e9
Nabin Maharjan <maharjan.nabin85@gmail.com>
2024-12-18 14:36:28 +0545

    fix: category, featured and bestselling component changed to server component

--- frontend/src/app/(client)/components/BestSelling.jsx
+++ frontend/src/app/(client)/components/BestSelling.jsx
@@ -1,25 +1,16 @@
-'use client'
-
 import axios from 'axios'
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
 import { IoMdHeartEmpty } from "react-icons/io";
 import { IoMdStar } from "react-icons/io";
 import ButtonComponent from './UI/ButtonComponent'
 
-const BestSelling = () => {
-  const router = useRouter()
-  const [productList, setProductList] = useState([]);
-
-  useEffect(() => {
-    const fetchFeaturedProduct = async () => {
-      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
-      setProductList(data.data.filter(item => item.isBestSelling))
-    }
-    fetchFeaturedProduct()
-  }, [])
+const BestSelling = async () => {
+  //fetch all product list
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+  const productList = await res.data
+  const bestSellingProducts = productList.data.filter(item => item.isBestSelling)
+  
   return (
     <div>
       <div className='container mx-auto pt-4 pb-12'>
@@ -28,32 +19,32 @@ const BestSelling = () => {
             <p className='text-lg sm:text-xl font-semibold'>Bestseller Products</p>
             <div className='w-[140px] h-[2px] bg-primaryColor'></div>
           </div>
-          {/* <p className='text-gray-400'>See All</p> */}
         </div>
         <div className=' w-full grid grid-cols-5 justify-between items-center gap-4 pt-4'>
-          {productList.map((item, index) => (
-            <div className='cursor-pointer border' onClick={() => router.push(`/shop?category=${item.productCategory}&subcategory=${item.productSubcategory}&product=${item.productName}`)}>
-              <div key={index} className='w-[235px] h-48 flex justify-center items-center overflow-hidden relative'>
-                <Image
-                  src={item.productImage}
-                  alt='Banner showcasing various bags available at COMO'
-                  width={2000}
-                  height={2000}
-                  className='absolute w-full h-full object-cover'
-                  priority={true}
-                />
-                <Link href="/"><IoMdHeartEmpty className='absolute top-2 left-2 text-primaryColor' /></Link>
-              </div>
-              <div className='p-2 border-t'>
-                <p className='text-base font-medium'>{item.productName}</p>
-                <p className='text-xs font-normal text-gray-400'>{item.productSubcategory}</p>
-                <div className='flex gap-1 text-primaryColor'><IoMdStar /><IoMdStar /><IoMdStar /><IoMdStar /><IoMdStar /></div>
-                <p className='text-base font-medium text-gray-800'>${item.productSellPrice}</p>
-
-                <ButtonComponent>Add To Cart</ButtonComponent>
-
-
-              </div>
+          {bestSellingProducts.map((item) => (
+            <div key={item.id} className='cursor-pointer border'>
+              <Link href={`/shop?category=${item.productCategory}&subcategory=${item.productSubcategory}&product=${item.productName}`}>
+                <div className='w-[235px] h-48 flex justify-center items-center overflow-hidden relative'>
+                  <Image
+                    src={item.productImage}
+                    alt={`Image of ${item.productName}`}
+                    width={2000}
+                    height={2000}
+                    className='absolute w-full h-full object-cover'
+                    priority={true}
+                  />
+                  <IoMdHeartEmpty className='absolute top-2 left-2 text-primaryColor' />
+                </div>
+                <div className='p-2 border-t'>
+                  <p className='text-base font-medium'>{item.productName}</p>
+                  <p className='text-xs font-normal text-gray-400'>{item.productSubcategory}</p>
+                  <div className='flex gap-1 text-primaryColor'>
+                    <IoMdStar /><IoMdStar /><IoMdStar /><IoMdStar /><IoMdStar />
+                  </div>
+                  <p className='text-base font-medium text-gray-800'>${item.productSellPrice}</p>
+                  <ButtonComponent>Add To Cart</ButtonComponent>
+                </div>
+              </Link>
             </div>
           ))}
         </div>
--- frontend/src/app/(client)/components/Featured.jsx
+++ frontend/src/app/(client)/components/Featured.jsx
@@ -1,26 +1,15 @@
-'use client'
-
 import axios from 'axios'
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
 import { IoMdStar } from "react-icons/io";
 import ButtonComponent from './UI/ButtonComponent'
 import { IoMdHeartEmpty } from "react-icons/io";
 import Link from 'next/link';
 
-const Featured = () => {
-    const router = useRouter()
-    const [productList, setProductList] = useState([]);
-    console.log(productList)
-
-    useEffect(() => {
-        const fetchFeaturedProduct = async () => {
-            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
-            setProductList(data.data.filter(item => item.isFeatured))
-        }
-        fetchFeaturedProduct()
-    }, [])
+const Featured = async () => {
+    //fetch all product list
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+    const productList = await res.data
+    const featuredProduct = productList.data.filter(item => item.isFeatured)
 
     return (
         <div className='bg-secondaryColor border-t'>
@@ -30,32 +19,32 @@ const Featured = () => {
                         <p className='text-lg sm:text-xl font-semibold'>Featured Products</p>
                         <div className='w-[90px] h-[2px] bg-primaryColor'></div>
                     </div>
-                    {/* <p className='text-gray-400'>See All</p> */}
                 </div>
                 <div className=' w-full py-8 grid grid-cols-5 justify-between items-center gap-4'>
-                    {productList.map((item, index) => (
-                        <div className='cursor-pointer border' onClick={() => router.push(`/shop?category=${item.productCategory}&subcategory=${item.productSubcategory}&product=${item.productName}`)}>
-                            <div key={index} className='w-[235px] h-48 flex justify-center items-center overflow-hidden relative'>
-                                <Image
-                                    src={item.productImage}
-                                    alt='Banner showcasing various bags available at COMO'
-                                    width={2000}
-                                    height={2000}
-                                    className='absolute w-full h-full object-cover'
-                                    priority={true}
-                                />
-                                <Link href="/"><IoMdHeartEmpty className='absolute top-2 left-2 text-primaryColor' /></Link>
-                            </div>
-                            <div className='p-2 border-t'>
-                                <p className='text-base font-medium'>{item.productName}</p>
-                                <p className='text-xs font-normal text-gray-400'>{item.productSubcategory}</p>
-                                <div className='flex gap-1 text-primaryColor'><IoMdStar /><IoMdStar /><IoMdStar /><IoMdStar /><IoMdStar /></div>
-                                <p className='text-base font-medium text-gray-800'>${item.productSellPrice}</p>
-
-                                <ButtonComponent>Add To Cart</ButtonComponent>
-                               
-
-                            </div>
+                {featuredProduct.map((item) => (
+                        <div key={item.id} className='cursor-pointer border'>
+                            <Link href={`/shop?category=${item.productCategory}&subcategory=${item.productSubcategory}&product=${item.productName}`}>
+                                <div className='w-[235px] h-48 flex justify-center items-center overflow-hidden relative'>
+                                    <Image
+                                        src={item.productImage}
+                                        alt={`Image of ${item.productName}`}
+                                        width={2000}
+                                        height={2000}
+                                        className='absolute w-full h-full object-cover'
+                                        priority={true}
+                                    />
+                                    <IoMdHeartEmpty className='absolute top-2 left-2 text-primaryColor' />
+                                </div>
+                                <div className='p-2 border-t'>
+                                    <p className='text-base font-medium'>{item.productName}</p>
+                                    <p className='text-xs font-normal text-gray-400'>{item.productSubcategory}</p>
+                                    <div className='flex gap-1 text-primaryColor'>
+                                        <IoMdStar /><IoMdStar /><IoMdStar /><IoMdStar /><IoMdStar />
+                                    </div>
+                                    <p className='text-base font-medium text-gray-800'>${item.productSellPrice}</p>
+                                    <ButtonComponent>Add To Cart</ButtonComponent>
+                                </div>
+                            </Link>
                         </div>
                     ))}
                 </div>
@@ -65,5 +54,21 @@ const Featured = () => {
 }
 
 export default Featured
+
+
+// const router = useRouter()
+//     const [productList, setProductList] = useState([]);
+//     console.log(productList)
+
+//     useEffect(() => {
+//         const fetchFeaturedProduct = async () => {
+//             const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+//             setProductList(data.data.filter(item => item.isFeatured))
+//         }
+//         fetchFeaturedProduct()
+//     }, [])
+
+
+// onClick={() => router.push(`/shop?category=${item.productCategory}&subcategory=${item.productSubcategory}&product=${item.productName}`)}
 
 
